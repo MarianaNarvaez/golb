00335352315fb382c8cb6e76979fb085a47aa18f
MarianaNarvaez <mnarvae3@eafit.edu.co>
2017-04-20 18:20:33 -0500

    AiRescue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
-Blog::Application.routes.draw do
-#Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'articles/new'
-  resources :articles
-  root 'welcome#index'
-  resources :articles do
-  	resources :comments
-  end
+Rails.application.routes.draw do
+  get 'ubications/index'
+  get 'variable/index'
+  root 'ubications#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
